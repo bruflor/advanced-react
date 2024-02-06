9e0c4a5db7281490f021759fa8871bd49998a960
Bruna <brn.flor@gmail.com>
2024-02-06 19:56:57 +0000

    updating import

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require("express");
+import express, { json } from "express";
 
 const app = express();
 
-app.use(express.json());
+app.use(json());
 
 let currentUser = {
   name: "Sarah Waters",
