c69c1f36bb61e30991546d451cd3a237465453d3
Bruna <brn.flor@gmail.com>
2024-02-06 20:27:52 +0000

    current config vite to express proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+ 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig ({
+  plugins: [react ()],
+  server: {
+    proxy: {
+      // Forward all requests starting with '/api' to your backend server
+      '/api': {
+        target: 'http://localhost:9090',
+        changeOrigin: true,
+        rewrite: path => path.replace (/^\/api/, ''),
+      },
+    },
+  },
+});
