2cd7c2921c6bdb519bddf5f05b71b26469dfd793
Bruna <brn.flor@gmail.com>
2024-02-27 20:24:21 +0000

    resource-loader component

---
+++ src/components/resource-loader.tsx
@@ -0,0 +1,24 @@
+import axios from "axios"
+import React, { ReactNode, useEffect, useState } from "react"
+
+export const ResourceLoader = ({resourceUrl, resourceName, children}: {resourceUrl: string, resourceName: string, children: ReactNode}) => {
+    const [resource, setResource] = useState(null)
+
+    useEffect(() => {
+        (async() => {
+            const response = await axios.get(resourceUrl);
+            setResource(response.data)
+        })()
+    }, [resourceUrl])
+    
+    return (
+        <>
+        {React.Children.map(children, (child) => {
+            if (React.isValidElement(child)) {
+                return React.cloneElement(child, {[resourceName]: resource})
+            }
+            return child
+        })}
+        </>
+    )
+}
