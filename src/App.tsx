064dfc40f2413c534f5e1e39ce99ed42f7d8f777
Bruna <brn.flor@gmail.com>
2024-05-18 13:18:51 +0100

    recursive component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import {UserInfoForm} from "./components/user-form.tsx";
+import {RecursiveComponent} from "./components/recursive.tsx";
 
 function App() {
-
+    const mockObject = {
+        id: 1,
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+        address: {
+            street: '123 Main St',
+            city: 'New York',
+            state: 'NY',
+            country: 'USA',
+            zipCode: '10001',
+            location: {
+                latitude: 40.7128,
+                longitude: -74.0060,
+                timezone: 'America/New_York',
+            },
+        },
+    };
   return (
     <>
-        <UserInfoForm />
+        <RecursiveComponent data={mockObject} />
     </>
      
   )
---
+++ src/components/recursive.tsx
@@ -0,0 +1,23 @@
+
+const isObject = (data:any) => typeof data === 'object' && data !== null;
+
+export const RecursiveComponent = ({data}:{data:any}) => {
+    if(!isObject(data)){
+        return <li>{data}</li>
+    }
+
+    const pais = Object.entries(data)
+    return (
+        <>
+        {pais.map(([key, value]) => (
+            <li key={key}>
+                {key} :
+                <ul>
+                    <RecursiveComponent data={value} />
+                </ul>
+            </li>
+        ))}
+        </>
+    )
+
+}
