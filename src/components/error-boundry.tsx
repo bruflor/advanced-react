4f656b263256c07838bf493b8969fe8d770ee1df
Bruna <brn.flor@gmail.com>
2024-06-29 19:46:30 +0100

    did catch console error

--- src/components/error-boundry.tsx
+++ src/components/error-boundry.tsx
@@ -2,12 +2,17 @@ import React from "react";
 
 export class ErrorBoundry extends React.Component{
     state = {hasError:false}
-    props = {fallback: <p>Testing error</p>}
+    props = {fallback: <p>Testing error</p>, children:<></>}
 
     static getDerivedStateFromError(error: unknown): any {
         console.error(error)
         return {hasError:true};
     }
+
+    componentDidCatch(error: Error) {
+        console.error(error)
+    }
+
     render(){
         if(this.state.hasError){
             return this.props.fallback;
